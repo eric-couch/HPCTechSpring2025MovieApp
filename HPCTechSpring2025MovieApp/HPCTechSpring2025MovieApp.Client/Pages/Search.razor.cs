595d9a40216b29d9ed8b877985d31c65d25b60c7
Eric Couch <ericcouch@yahoo.com>
2025-04-02 14:44:27 -0500

    added search navigation and grid

--- HPCTechSpring2025MovieApp/HPCTechSpring2025MovieApp.Client/Pages/Search.razor.cs
+++ HPCTechSpring2025MovieApp/HPCTechSpring2025MovieApp.Client/Pages/Search.razor.cs
@@ -1,5 +1,7 @@
 ﻿using HPCTechSpring2025MovieApp.Shared;
 using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Grids;
+using Syncfusion.Blazor.Navigations;
 using System.Net.Http.Json;
 
 namespace HPCTechSpring2025MovieApp.Client.Pages;
@@ -10,9 +12,32 @@ public partial class Search
     public HttpClient Http { get; set; }
     private string searchTerm;
     public MovieSearchResultDto searchResult { get; set; }
+    public List<MovieSearchResultItemDto> OMDBMovies { get; set; } = new();
+    public MovieSearchResultItemDto SelectedMovie { get; set; }
+    public MovieDto? omdbMovie { get; set; }
+    private int page { get; set; } = 1;
+    private int totalItems { get; set; } = 0;
+
+    public async Task GetSelectedRow(RowSelectEventArgs<MovieSearchResultItemDto> args)
+    {
+        SelectedMovie = args.Data;
+        omdbMovie = await Http.GetFromJsonAsync<MovieDto>($"api/GetMovie?imdbID={SelectedMovie.imdbID}");
+
+    }
+
+    public async Task PageClick(PagerItemClickEventArgs args)
+    {
+        page = args.CurrentPage;
+        await SearchOMDB();
+    }
 
     private async Task SearchOMDB()
     {
-        searchResult = await Http.GetFromJsonAsync<MovieSearchResultDto>($"api/SearchMovies?searchTerm={searchTerm}");
+        searchResult = await Http.GetFromJsonAsync<MovieSearchResultDto>($"api/SearchMovies?searchTerm={searchTerm}&page={page}");
+        if (searchResult?.Search?.Any() ?? false)
+        {
+            totalItems = Int32.Parse(searchResult.totalResults);
+            OMDBMovies = searchResult.Search;
+        }
     }
 }
--- HPCTechSpring2025MovieApp/HPCTechSpring2025MovieApp/Controllers/MovieController.cs
+++ HPCTechSpring2025MovieApp/HPCTechSpring2025MovieApp/Controllers/MovieController.cs
@@ -6,12 +6,13 @@ namespace HPCTechSpring2025MovieApp.Controllers;
 public class MovieController : Controller
 {
     private readonly HttpClient _httpClient;
+    private readonly IConfiguration _config;
     private readonly string OMDBAPIUrl = "http://www.omdbapi.com/?";
-    private readonly string OMDBAPIKey = "apikey=86c39163";
 
-    public MovieController(HttpClient httpClient)
+    public MovieController(HttpClient httpClient, IConfiguration config)
     {
         _httpClient = httpClient;
+        _config = config;
     }
 
 
@@ -19,12 +20,27 @@ public class MovieController : Controller
     [Route("api/SearchMovies")]
     public async Task<IActionResult> SearchOMDBMovies(string searchTerm, int page = 1)
     {
-        var searchResult = await _httpClient.GetFromJsonAsync<MovieSearchResultDto>($"{OMDBAPIUrl}{OMDBAPIKey}&s={searchTerm}");
+        string OMDBAPIKey = _config["Movies:OMDBAPIKey"];
+        var searchResult = await _httpClient.GetFromJsonAsync<MovieSearchResultDto>($"{OMDBAPIUrl}apikey={OMDBAPIKey}&s={searchTerm}&page={page}");
         if (searchResult?.Search?.Any() ?? false)
         {
             return Ok(searchResult);
         }
         else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpGet]
+    [Route("api/GetMovie")]
+    public async Task<IActionResult> GetOMDBMovie(string imdbID)
+    {
+        string OMDBAPIKey = _config["Movies:OMDBAPIKey"];
+        MovieDto movie = await _httpClient.GetFromJsonAsync<MovieDto>($"{OMDBAPIUrl}apikey={OMDBAPIKey}&i={imdbID}");
+        if (movie != null) { 
+            return Ok(movie);
+        } else
         {
             return NotFound();
         }
